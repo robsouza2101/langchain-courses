470ab6f5512fdb0d4b334915a56c26622bfe56bb
Robson <robsouza2101@gmail.com>
2025-11-18 20:36:01 -0300

    Separate what necessary info the selected llm needs and comment what it does not need

--- main.py
+++ main.py
@@ -7,17 +7,18 @@ load_dotenv()
 
 def main():
     print("Hello from langchain-course!")
-    information = """
-    Philippe Coutinho Correia (Rio de Janeiro, 12 de junho de 1992) é um futebolista brasileiro que atua como meio-campista no Vasco da Gama.
 
-    Uma das grandes revelações do Vasco no século XXI, Philippe Coutinho foi contratado pela Internazionale em 2008, com apenas 16 anos, mas continuou no clube carioca e realizou sua estreia como profissional no ano de 2009. O meia seguiu para a Inter em 2010, após completar 18 anos, mas teve poucas chances e foi emprestado ao Espanyol.
+    # Template to summarize information about a person (ChatOllama)
+    # information = """
+    # Philippe Coutinho Correia (Rio de Janeiro, 12 de junho de 1992) é um futebolista brasileiro que atua como meio-campista no Vasco da Gama.
 
-    Em janeiro de 2013 foi negociado com o Liverpool, onde viveu seu auge e tornou-se um dos melhores jogadores do futebol europeu. Em janeiro de 2018, Coutinho assinou com o Barcelona por 135 milhões de euros, sendo a quarta transferência mais cara da história do futebol. Emprestado ao Bayern de Munique na temporada 2019–20, o meia não foi titular absoluto na equipe, mas era constantemente utilizado e fez parte da conquista da tríplice coroa. Coutinho retornou ao futebol inglês em janeiro de 2022, inicialmente sendo emprestado ao Aston Villa. Após reencontrar o bom futebol na equipe, assinou em definitivo com os Lions.
+    # Uma das grandes revelações do Vasco no século XXI, Philippe Coutinho foi contratado pela Internazionale em 2008, com apenas 16 anos, mas continuou no clube carioca e realizou sua estreia como profissional no ano de 2009. O meia seguiu para a Inter em 2010, após completar 18 anos, mas teve poucas chances e foi emprestado ao Espanyol.
 
-    Pela Seleção Brasileira, o jogador estreou em 2010 e disputou mais de 60 partidas. Convocado para três edições da Copa América, foi campeão da competição em 2019, sendo um dos principais destaques do título em solo brasileiro. Além disso, também representou a Amarelinha na Copa do Mundo de 2018.    
-    """
+    # Em janeiro de 2013 foi negociado com o Liverpool, onde viveu seu auge e tornou-se um dos melhores jogadores do futebol europeu. Em janeiro de 2018, Coutinho assinou com o Barcelona por 135 milhões de euros, sendo a quarta transferência mais cara da história do futebol. Emprestado ao Bayern de Munique na temporada 2019–20, o meia não foi titular absoluto na equipe, mas era constantemente utilizado e fez parte da conquista da tríplice coroa. Coutinho retornou ao futebol inglês em janeiro de 2022, inicialmente sendo emprestado ao Aston Villa. Após reencontrar o bom futebol na equipe, assinou em definitivo com os Lions.
+
+    # Pela Seleção Brasileira, o jogador estreou em 2010 e disputou mais de 60 partidas. Convocado para três edições da Copa América, foi campeão da competição em 2019, sendo um dos principais destaques do título em solo brasileiro. Além disso, também representou a Amarelinha na Copa do Mundo de 2018.    
+    # """
     
-    # Template to summarize information
     # summary_template = """
     # Dado a informação: {information} sobre uma pessoa, eu quero que vc elabore:
     # 1. Um resumo curto;
